415fdcc06966a6694a5e574a0b156dcbbf21ac79
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2021-07-29 19:49:29 -0300

    add update cadastrarUsuario function

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -9,40 +9,31 @@ const cadastrarUsuario = async (req, res) => {
     await schemaCadastroUsuario.validate(req.body);
 
     const usuarioEncontrado = await knex('usuario').where({ email }).first();
-    if (usuarioEncontrado) return res.status(404).json({ erro: 'Este email ja está cadastrado' });
+    if (usuarioEncontrado) return res.status(409).json({ erro: 'Este email ja está cadastrado' });
 
     const cryptSenha = await bcrypt.hash(senha, 10);
 
-    const novoUsuario = () => {
-      try {
-        return knex('usuario')
-          .insert({ nome, email, senha: cryptSenha })
-          .returning('id')
-          .then((res) => {
-            return knex('restaurante')
-              .insert({
-                usuario_id: res[0],
-                nome: restaurante.nome,
-                descricao: restaurante.descricao,
-                categoria_id: restaurante.idCategoria,
-                taxa_entrega: restaurante.taxaEntrega,
-                tempo_entrega_minutos: restaurante.tempoEntregaEmMinutos,
-                valor_minimo_pedido: restaurante.valorMinimoPedido
-              })
-          });
-      } catch (error) {
-        return res.status(400).json(error.message);
-      }
-    }
 
-    novoUsuario();
+    knex('usuario')
+      .insert({ nome, email, senha: cryptSenha })
+      .returning('id')
+      .then((res) => {
+        return knex('restaurante')
+          .insert({
+            usuario_id: res[0],
+            nome: restaurante.nome,
+            descricao: restaurante.descricao,
+            categoria_id: restaurante.idCategoria,
+            taxa_entrega: restaurante.taxaEntrega,
+            tempo_entrega_minutos: restaurante.tempoEntregaEmMinutos,
+            valor_minimo_pedido: restaurante.valorMinimoPedido
+          })
+      });
 
-    return res.status(200).json({ sucesso: 'Usuário cadastrado com sucesso!' });
+    return res.sendStatus(200);
   } catch (error) {
     return res.status(400).json(error.message);
   }
-
 };
-
 
 module.exports = { cadastrarUsuario };
